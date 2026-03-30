b4debdb45260dd2c4aa1105b2c4c346f9ca8763b
Kris Sreeramakavacham <svkrishna6342@gmail.com>
2026-03-29 20:30:25 -0400

    SQL

--- document_middleware/INITIALIZE.sql
+++ document_middleware/INITIALIZE.sql
@@ -11,7 +11,7 @@ CREATE TABLE `admin` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-insert into admin values(1, '123 Main Street, New York, NY', 'vs787@njit.edu', 'Kris Sreeramakavacham', 'cyrus.com', '1234567890', 'kris.admin');
+insert into admin values(1, '123 Main Street, New York, NY', 'alice.admin@gmail.com', 'Alice', 'alice', '1234567890', 'alice.admin');
 
 CREATE TABLE `client` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -24,9 +24,8 @@ CREATE TABLE `client` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-insert into client values(1, "123 Main Street, New York, NY","svkrishna6342@gmail.com","Kris Sreeramakavacham","cyrus.com", "1234567890","kris.user");
-insert into client values(2, "123 Main Street, New York, NY","shantanu.sharma@njit.edu","Shantanu","shantanu", "1234567890","shantanu");
-insert into client values(3, "123 Main Street, New York, NY","kk675@njit.edu","Komal","komal", "1234567890","komal");
-
+insert into client values(1, "123 Main Street, New York, NY","alice@gmail.com","Alice","alice", "1234567890","alice.user");
+insert into client values(2, "123 Main Street, New York, NY","bob@gmail.com","bob","bob", "1234567890","bob.user");
+insert into client values(3, "123 Main Street, New York, NY","steve@gmail.com","Steve","steve", "1234567890","steve.user");
 select * from admin;
 select * from client;
